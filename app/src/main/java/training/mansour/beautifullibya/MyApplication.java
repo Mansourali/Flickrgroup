43f7cfdb95ace0a7b8422f9d6648eb7ba985709a
Mansour Alssager <mansour.alssager@gmail.com>
2016-02-13 01:26:37 +0800

    Update MyApplication.java

--- app/src/main/java/training/mansour/beautifullibya/MyApplication.java
+++ app/src/main/java/training/mansour/beautifullibya/MyApplication.java
@@ -14,7 +14,7 @@ public class MyApplication extends Application {
 
     private static MyApplication mInstance;
     private static ImagesDatabase FlickrImageDB ;
-    public static final String FLICKR_API_KEY = "ee91e6f9cedcf5a3f9951d69671117c0";
+    public static final String FLICKR_API_KEY = "";
 
     @Override
     public void onCreate() {
